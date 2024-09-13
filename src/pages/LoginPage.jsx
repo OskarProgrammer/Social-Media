f9ba9f0e88df8010ccbb4f48c33194aa86732bf4
Oskar <Zaden>
2024-09-13 12:58:45 +0200

    Creating Links to form and reg form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,28 @@
 
 // importing functions and components from react library
-import { Form } from "react-router-dom"
+import { Form, Link } from "react-router-dom"
 
 
 export const LoginPage = () => {
 
 
     return (
-        <Form className="form">
+        <Form method="POST" className="form">
 
             <h2 className="titleOfForm">Login form</h2>
 
-            <input type="text" className="inputField" placeholder="Login"/>
+            <input type="text" className="inputField" placeholder="Login" name="login"/>
 
-            <input type="password" className="inputField" placeholder="Password"/>
+            <input type="password" className="inputField" placeholder="Password" name="password"/>
+
+            <p className="informationTag"> 
+                Haven't got account yet ?  
+
+                <Link to="/register" className="linkTo">
+                    Click here
+                </Link>
+
+            </p>
 
             <button className="btn-green mx-auto">Sign in</button>
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 
 // importing funcitons and components from react library
-import { Form } from "react-router-dom"
+import { Form, Link } from "react-router-dom"
 
 export const RegisterPage = () => {
 
@@ -14,6 +14,15 @@ export const RegisterPage = () => {
             <input type="password" className="inputField" placeholder="Password"/>
 
             <input type="password" className="inputField" placeholder="Confirm password"/>
+
+            <p className="informationTag"> 
+                Have got account already ?  
+
+                <Link to="/login" className="linkTo">
+                    Click here
+                </Link>
+
+            </p>
 
             <button className="btn-green mx-auto">Sign up</button>
 
